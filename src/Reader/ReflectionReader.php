768fca231381d1eafedb8770ecf67f1ae8518535
Rafael SR <raf.rsr@gmail.com>
2016-06-23 10:30:10 -0400

    reflection reader

---
+++ src/Reader/ReflectionReader.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * LICENSE: This file is subject to the terms and conditions defined in
+ * file 'LICENSE', which is part of this source code package.
+ *
+ * @copyright 2016 Copyright(c) - All rights reserved.
+ */
+
+namespace Rafrsr\LibArray2Object\Reader;
+
+class ReflectionReader implements PropertyReaderInterface
+{
+    protected $onlyPublicProperties;
+
+    /**
+     * @param boolean $onlyPublicProperties ignore private or protected properties
+     */
+    public function __construct($onlyPublicProperties = false)
+    {
+        $this->onlyPublicProperties = $onlyPublicProperties;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isReadable($object, $propertyPath)
+    {
+        $classRef = new \ReflectionClass(get_class($object));
+
+        if (!$classRef->hasProperty($propertyPath)) {
+            return false;
+        }
+
+        $property = $classRef->getProperty($propertyPath);
+        if ($this->onlyPublicProperties && !$property->isPublic()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getValue($object, $propertyPath)
+    {
+        if ($this->isReadable($object, $propertyPath)) {
+            $property = new \ReflectionProperty(get_class($object), $propertyPath);
+            if (!$property->isPublic()) {
+                $property->setAccessible(true);
+            }
+
+            return $property->getValue($object);
+        }
+
+        return null;
+    }
+}
---
+++ tests/Reader/ReflectionReaderTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * LICENSE: This file is subject to the terms and conditions defined in
+ * file 'LICENSE', which is part of this source code package.
+ *
+ * @copyright 2016 Copyright(c) - All rights reserved.
+ */
+
+namespace Rafrsr\LibArray2Object\Tests\Reader;
+
+use Rafrsr\LibArray2Object\Reader\ReflectionReader;
+use Rafrsr\LibArray2Object\Tests\Fixtures\Team;
+
+class ReflectionReaderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testIsReadable()
+    {
+        $reader = new ReflectionReader();
+        static::assertTrue($reader->isReadable(new Team(), 'name'));
+        static::assertFalse($reader->isReadable(new Team(), 'none'));
+
+        $reader = new ReflectionReader(true);
+        static::assertFalse($reader->isReadable(new Team(), 'name'));
+    }
+
+    public function testGetValue()
+    {
+        $reader = new ReflectionReader();
+        $team = new Team();
+        $team->setName('New Name');
+        static::assertEquals('New Name', $reader->getValue($team, 'name'));
+
+        $reader = new ReflectionReader(true);
+        static::assertNull($reader->getValue($team, 'name'));
+    }
+}
